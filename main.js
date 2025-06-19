c4410057a703186dcd6e8f84f125a47b976974d5
devdanig <danielgfdev@gmail.com>
2025-06-18 23:15:47 -0500

    [UPDATE] Se modifico el main, se estructuro de nuevo para mostrar llamar 3 imaganes

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-const URL = 'https://api.nasa.gov/planetary/apod?api_key=nDBYkod6BtZONrkEmYH9sdsDaElt7IMswk6rGJmL&count=1';
+const API_URL = 'https://api.nasa.gov/planetary/apod?api_key=nDBYkod6BtZONrkEmYH9sdsDaElt7IMswk6rGJmL&count=3';
 
-async function pod() {
-    const respuesta = await fetch(URL);
-    const data = await respuesta.json();
-    const img = document.querySelector('img');
-    const title = document.querySelector('h2');
-    const date = document.getElementById('date');
-    img.src = data[0].url;
-    title.textContent = data[0].title;
-    date.textContent = data[0].date;
+async function pod(){
+    const response = await fetch(API_URL)
+    const data = await response.json()
+    console.log(data)
+
+    const date1 = document.getElementById('date1')
+    const date2 = document.getElementById('date2')
+    const date3 = document.getElementById('date3')
+
+    const img1 = document.getElementById('img1')
+    const img2 = document.getElementById('img2')
+    const img3 = document.getElementById('img3')
+
+    date1.innerHTML = data[0].date
+    date2.innerHTML = data[1].date
+    date3.innerHTML = data[2].date
+
+    img1.src = data[0].url
+    img2.src = data[1].url
+    img3.src = data[2].url
 }
+
 pod()
-const btn = document.querySelector('button');
-btn.addEventListener('click', pod);
